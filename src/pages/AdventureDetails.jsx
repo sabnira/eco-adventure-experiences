24c6d83e772a44cffb306a3c96d3a7e0f6241a56
sabnira <sabnirajeemi@gmail.com>
2025-08-11 00:59:23 +0600

    talk with expert and book now button

--- src/pages/AdventureDetails.jsx
+++ src/pages/AdventureDetails.jsx
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar';
 import Footer from "../components/Footer";
 import { IoCheckmarkDoneSharp } from "react-icons/io5";
 import { RiErrorWarningLine } from "react-icons/ri";
+import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
 
 const AdventureDetails = () => {
 
@@ -19,6 +20,29 @@ const AdventureDetails = () => {
 
 
     const { title, image, shortDescription, location, duration, includedItems, ecoFriendlyFeatures, specialInstructions, maxGroupSize, adventureLevel, adventureCost } = adventure;
+
+    const handleClick = () => {
+
+        const now = new Date();
+        const hour = now.getHours();
+
+        // const hour = 11;
+
+        if (hour >= 10 && hour < 20) {
+            window.open("https://meet.google.com/landing", "_blank");
+        } else {
+
+            const modal = document.getElementById("consultation_modal");
+            if (modal)
+                modal.showModal();
+        }
+    };
+
+    const handleBooking = () => {
+        const modal = document.getElementById("bookNow_modal");
+            if (modal)
+                modal.showModal();
+    }
 
 
     return (
@@ -72,13 +96,13 @@ const AdventureDetails = () => {
                                     <td className="text-2xl">Included Items:</td>
                                     <td>
                                         <ul>
-                                        {
-                                            includedItems?.map((items, idx) => (<li key={idx}>
-                                                <div className="flex items-center gap-2 text-lg"> 
-                                                    <IoCheckmarkDoneSharp className="text-green-700"></IoCheckmarkDoneSharp>{items}
-                                                </div>
-                                            </li>))
-                                        }
+                                            {
+                                                includedItems?.map((items, idx) => (<li key={idx}>
+                                                    <div className="flex items-center gap-2 text-lg">
+                                                        <IoCheckmarkDoneSharp className="text-green-700"></IoCheckmarkDoneSharp>{items}
+                                                    </div>
+                                                </li>))
+                                            }
                                         </ul>
                                     </td>
                                 </tr>
@@ -87,13 +111,13 @@ const AdventureDetails = () => {
                                     <td className="text-2xl">Eco Friendly Features:</td>
                                     <td>
                                         <ul>
-                                        {
-                                            ecoFriendlyFeatures?.map((items, idx) => (<li key={idx}>
-                                                <div className="flex items-center gap-2 text-lg"> 
-                                                    <IoCheckmarkDoneSharp className="text-green-700"></IoCheckmarkDoneSharp>{items}
-                                                </div>
-                                            </li>))
-                                        }
+                                            {
+                                                ecoFriendlyFeatures?.map((items, idx) => (<li key={idx}>
+                                                    <div className="flex items-center gap-2 text-lg">
+                                                        <IoCheckmarkDoneSharp className="text-green-700"></IoCheckmarkDoneSharp>{items}
+                                                    </div>
+                                                </li>))
+                                            }
                                         </ul>
                                     </td>
                                 </tr>
@@ -102,13 +126,13 @@ const AdventureDetails = () => {
                                     <td className="text-2xl">Special Instructions:</td>
                                     <td>
                                         <ul>
-                                        {
-                                            specialInstructions?.map((items, idx) => (<li key={idx}>
-                                                <div className="flex items-center gap-2 text-lg"> 
-                                                    <RiErrorWarningLine className="text-yellow-500"></RiErrorWarningLine>{items}
-                                                </div>
-                                            </li>))
-                                        }
+                                            {
+                                                specialInstructions?.map((items, idx) => (<li key={idx}>
+                                                    <div className="flex items-center gap-2 text-lg">
+                                                        <RiErrorWarningLine className="text-yellow-500"></RiErrorWarningLine>{items}
+                                                    </div>
+                                                </li>))
+                                            }
                                         </ul>
                                     </td>
                                 </tr>
@@ -118,14 +142,50 @@ const AdventureDetails = () => {
                     </div>
 
                     <div>
-                        <button className="btn px-16 py-6 bg-[#D5E880] border-none shadow-none text-lg rounded-br-3xl hover:bg-[#2A445E] hover:text-white hover:rounded-br-sm transition-all duration-300 ease-in-out">
-                                Talk with Expert
+                        <button onClick={handleBooking} className="btn px-16 py-6 bg-[#D5E880] border-none shadow-none mr-2 mb-2 md:mb-0 text-lg rounded-br-3xl hover:bg-[#2A445E] hover:text-white hover:rounded-br-sm transition-all duration-300 ease-in-out">
+                            Book Now
+                        </button>
+                        <button onClick={handleClick} className="btn px-16 py-6 bg-[#D5E880] border-none shadow-none text-lg rounded-br-3xl hover:bg-[#2A445E] hover:text-white hover:rounded-br-sm transition-all duration-300 ease-in-out">
+                            Talk with Expert
                         </button>
                     </div>
 
                 </div>
 
             </div>
+
+            <dialog
+                id="bookNow_modal"
+                className="modal modal-bottom sm:modal-middle"
+            >
+                <div className="modal-box text-center mx-auto">
+                    <p className="text-5xl text-green-700 flex justify-center"><IoCheckmarkDoneCircleSharp></IoCheckmarkDoneCircleSharp></p>
+                    <p className="text-xl font-medium">Your booking has been confirmed!</p>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+
+            <dialog
+                id="consultation_modal"
+                className="modal modal-bottom sm:modal-middle"
+            >
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Consultation Time</h3>
+                    <p className="py-4">
+                        Our experts are available from <strong>10:00 AM</strong> to{" "}
+                        <strong>8:00 PM</strong>. Please try again during these hours.
+                    </p>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
 
             <Footer></Footer>
         </div>
