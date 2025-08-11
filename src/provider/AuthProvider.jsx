95056db1f03882b86cbfd66356cc8daaa9b4fbca
sabnira <sabnirajeemi@gmail.com>
2025-08-11 20:55:49 +0600

    context api and env setup

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,19 @@
+import { AuthContext } from "./AuthContext";
+
+
+
+const AuthProvider = ({children}) => {
+
+
+    const authInfo = {
+        
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
